fee7b3e07377ed961237856183ac74f425bde9b3
EseAlli <essealli.ea@gmail.com>
2022-06-25 18:03:20 +0100

    activate mandate ui done

--- src/components/ActivateMandate.jsx
+++ src/components/ActivateMandate.jsx
@@ -40,24 +40,28 @@ export default function ActivateMandate() {
     );
   }
   return (
-    <div>
+    <div className="flex h-[800px] justify-center">
       <div className="flex justify-center items-center p-4">
-        <form className="w-1/2 h-1/2 m-auto" onSubmit={handleActivateMandate}>
-          <div className="rounded-md shadow-sm -space-y-px">
+        <form className="m-auto" onSubmit={handleActivateMandate}>
+          <div className="rounded-md shadow-sm -space-y-px p-5">
             <h1 className="text-lg font-semibold p-2">Activate Mandate</h1>
-            <div className="flex flex-col justify-between capitalize">
-              <div className="flex justify-around flex-1">
-                <label className="">{fields ? fields.fieldone : ""}</label>
+            <div className="capitalize flex flex-col justify-center">
+              <div className="flex justify-center gap-4 mb-4">
+                <label className="w-[150px] text-left">
+                  {fields ? fields.fieldone : ""}
+                </label>
                 <input
-                  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 inline p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light placeholder:italic placeholder:text-slate-400"
+                  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 inline p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light placeholder:capitalize placeholder:text-slate-400"
                   name="fieldone"
                   placeholder={fields ? fields.fieldone : ""}
                   required
                   onChange={(e) => handleChange(e)}
                 />
               </div>
-              <div className="flex justify-around flex-1">
-                <label className="">{fields ? fields.fieldtwo : ""}</label>
+              <div className="flex justify-center gap-4 mb-4">
+                <label className="w-[150px] text-left">
+                  {fields ? fields.fieldtwo : ""}
+                </label>
                 <input
                   className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 inline p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light placeholder:text-slate-400"
                   name="fieldtwo"
@@ -67,7 +71,7 @@ export default function ActivateMandate() {
                 />
               </div>
             </div>
-            <div>
+            <div className="mb-4 p-4">
               <button
                 type="submit"
                 className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
