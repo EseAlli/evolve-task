231c769eccc3d8bd1bf144d8d5a43b6024ac8e4e
EseAlli <essealli.ea@gmail.com>
2022-06-25 17:15:24 +0100

    created transaction context

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import ActivateMandate from './components/ActivateMandate'
+
+import {TransactionProvider} from './context/TransactionContext'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TransactionProvider>
+        <ActivateMandate/>
+      </TransactionProvider>
     </div>
   );
 }
