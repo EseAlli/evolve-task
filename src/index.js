5edfb8736779398443aca38d1acdece7cb062ae5
Eseoghene <essealli.ea@gmail.com>
2022-06-26 01:09:25 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,10 +6,3 @@ import App from './App';
 export * from './context/TransactionContext';
 export * from './components/ActivateMandate';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
